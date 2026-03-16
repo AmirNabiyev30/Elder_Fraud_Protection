ff3702d84a876a0a58a19638587de3c7da3a9aa7
AmirN347 <amirn2203@gmail.com>
2026-03-16 13:34:52 -0400

    feat: Added a UI for the extensions

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,13 @@
-console.log("This is a popup!")
+console.log("Popup")
+
+
+const autoScanToggle = document.getElementById('autoScanToggle');
+const statusDot = document.querySelector('.status-dot');
+
+autoScanToggle.addEventListener('change', () => {
+  if (autoScanToggle.checked) {
+    statusDot.style.background = '#C9C9C9';
+  } else {
+    statusDot.style.background = '#27a065';
+  }
+});
