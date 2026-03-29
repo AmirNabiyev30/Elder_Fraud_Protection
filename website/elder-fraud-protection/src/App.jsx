315bb70d7a4fcdf5071551f1e547fe9479ed6a98
AmirN347 <amirn2203@gmail.com>
2026-03-29 18:47:39 -0400

    feat: Added website configs and created basic UI structure

---
+++ website/elder-fraud-protection/src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from './components/Navbar'
+import HeroSection from './components/Herosection'
+import ScanSection from './components/ScanSection'
+import Footer from './components/Footer'
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <HeroSection/>
+      <ScanSection/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
