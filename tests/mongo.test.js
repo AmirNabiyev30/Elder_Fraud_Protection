12790b2291b3d7f60e9d4a7749b8e3410e3811f7
Weizus <marcley.colin@aol.com>
2026-04-09 20:20:30 -0400

    fixed connection issue finally + updates from group repo

--- tests/mongo.test.js
+++ tests/mongo.test.js
@@ -5,6 +5,13 @@
 const BASE_URL = process.env.FLASK_URL || 'http://127.0.0.1:5000';
 
 describe('MongoDB Atlas Integration', () => {
+  test('Flask server should be reachable and responsive', async () => {
+    const start = Date.now();
+    await fetch(`${BASE_URL}/`);
+    const duration = Date.now() - start;
+    
+    expect(duration).toBeLessThan(2000);
+  });
   
   test('Flask should return a successful MongoDB connection status', async () => {
     // We can change the end of that url to test different "endpoints"
@@ -18,13 +25,5 @@ describe('MongoDB Atlas Integration', () => {
         status: "online",
         database: "connected" 
     });
-  });
-
-  test('Database should be reachable and responsive', async () => {
-    const start = Date.now();
-    await fetch(`${BASE_URL}/`);
-    const duration = Date.now() - start;
-    
-    expect(duration).toBeLessThan(2000);
   });
 });
