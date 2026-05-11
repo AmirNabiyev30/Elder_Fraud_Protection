735f90e03bcadc9e68f436934257a52d6a9827ec
AmirNabiev30 <anabiye000@citymail.cuny.edu>
2026-05-11 01:55:59 -0400

    Changed test so it allows auth when rendering home page

--- website/src/website-tests/Home.test.jsx
+++ website/src/website-tests/Home.test.jsx
@@ -1,6 +1,15 @@
 import Home from "../pages/Home";
 import {render} from "@testing-library/react"
 import { BrowserRouter } from "react-router-dom";
+import { vi } from "vitest";
+
+vi.mock("@clerk/clerk-react", () => ({
+  useAuth: () => ({
+    getToken: async () => "test-clerk-token",
+    isLoaded: true,
+    isSignedIn: true,
+  }),
+}));
 
 test('renders Home page', () =>{
     render(
